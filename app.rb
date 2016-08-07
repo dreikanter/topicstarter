7aa94e663c2a4e12266152cd8f1923b501ed4c1d
Alex Musayev <alex.musayev@gmail.com>
2016-08-08 01:04:27 +0300

    Using RestClient

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
+require 'rest-client'
 require 'sinatra'
 
 QUESTIONS_SOURCE_URL = ENV['QUESTIONS_SOURCE_URL']
-QUESTIONS = open(QUESTIONS_SOURCE_URL).readlines.map(&:strip).reject(&:empty?)
+QUESTIONS = RestClient.get(QUESTIONS_SOURCE_URL).body.split("\n").map(&:strip).reject(&:empty?)
 
 get '/' do
   erb :index, locals: { question: QUESTIONS.sample }
