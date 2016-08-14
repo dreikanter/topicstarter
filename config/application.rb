1ee7277413ee4bf544659e2bf360ffce42690bdd
Alex Musayev <alex.musayev@gmail.com>
2016-08-14 19:29:31 +0300

    Rails configuration

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,30 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Topicstarter
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %w(app lib).map { |p| Rails.root.join p }
+
+    config.active_job.queue_adapter = :delayed_job
+
+    config.generators do |g|
+      g.test_framework :minitest, spec: false, fixture: false
+      g.helper       false
+      g.decorator    false
+      g.assets       false
+      g.stylesheets  false
+      g.javascripts  false
+      g.view_specs   false
+      g.helper_specs false
+      g.skip_routes  true
+    end
+
+    config.lograge.enabled = true
+    config.lograge.custom_options = lambda do |event|
+      params = event.payload[:params].except('controller', 'action')
+      { params: params } unless params.empty?
+    end
   end
 end
