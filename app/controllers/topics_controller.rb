b8cdc433fc7c0905bd91743cb787d114e9b7bd33
Alex Musayev <alex.musayev@gmail.com>
2019-08-04 16:43:05 +0300

    Remove topics initializer

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,30 @@
 class TopicsController < ApplicationController
   def new
-    @question = QUESTIONS.sample
+    @question = questions.sample
     @secret = params[:secret]
     return unless @secret.present?
     ActionCable.server.broadcast("topics:#{@secret}", question: @question)
+  end
+
+  private
+
+  def questions
+    @questions ||= load_questions
+      .split("\n")
+      .map { |question| question.strip.gsub(/^\d+\.\s+/, '') }
+      .reject(&:empty?)
+      .freeze
+  end
+
+  def load_questions
+    unless Rails.env.production?
+      return File.read(Rails.root.join('db/questions.txt'))
+    end
+
+    RestClient.get(source_url).body
+  end
+
+  def source_url
+    ENV.fetch('QUESTIONS_SOURCE_URL')
   end
 end
